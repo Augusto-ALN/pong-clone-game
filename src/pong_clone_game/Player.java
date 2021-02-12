5bd334b8dd4bedef2ff31af2e25167a6ee31c59c
Augusto Alves <augustoaln20@gmail.com>
2021-02-12 16:28:55 -0300

    Added Enemy and ball with simple collision

--- src/pong_clone_game/Player.java
+++ src/pong_clone_game/Player.java
@@ -7,10 +7,13 @@ public class Player {
   
   public boolean right,left;
   public int x,y;
+  public int width,height;
   
   public Player(int x, int y) {
     this.x = x;
     this.y = y;
+    this.width = 40;
+    this.height = 10;
   }
   
   public void tick() {
@@ -20,10 +23,18 @@ public class Player {
     else if(left) {
       x--;
     }
+    
+    if(x+width > Game.WIDTH)
+    {
+      x = Game.WIDTH - width;
+    }
+    else if (x < 0) {
+      x = 0;
+    }
   }
   
   public void render(Graphics g) {
     g.setColor(Color.blue);
-    g.fillRect(x, y, 40, 10);
+    g.fillRect(x, y, width, height);
   }
 }
